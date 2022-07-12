9b87a28f4125fb5f01eac308e55afbf5dcbe3d53
00sleng00 <roman.morshikoff@yandex.ru>
2022-07-12 22:55:00 +0300

    Этап 3: Вестка

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,50 @@
+import './Footer.css'
+
+function Footer() {
+    return (
+        <footer className="footer">
+            <h3 className="footer__title">
+                Учебный проект Яндекс.Практикум х BeatFilm.
+            </h3>
+            <div className="footer__content">
+                <p className="footer__copyright">
+                    &copy; {new Date().getFullYear()}
+                </p>
+                <ul className="footer__links-list">
+                    <li className="footer__links-item">
+                        <a
+                            className="footer__link"
+                            href="https://practicum.yandex.ru"
+                            target="_blank"
+                            rel="noreferrer"
+                        >
+                            Яндекс.Практикум
+                        </a>
+                    </li>
+                    <li className="footer__links-item">
+                        <a
+                            className="footer__link"
+                            href="https://github.com/00sleng00"
+                            target="_blank"
+                            rel="noreferrer"
+                        >
+                            Github
+                        </a>
+                    </li>
+                    <li className="footer__links-item">
+                        <a
+                            className="footer__link"
+                            href="https://instagram.com/roman_morshikoff"
+                            target="_blank"
+                            rel="noreferrer"
+                        >
+                            Instagram
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
